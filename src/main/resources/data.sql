b1b894846964dc23561c99289c4282f06ac507dd
pavel astukov <p7ll7p@google.com>
2024-03-10 14:58:14 +0400

    change init and http query data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,17 +1,17 @@
 delete from rule;
 
 insert into rule(expected_status_code, is_activated, period_in_seconds, url)
-values (200, true, 10, 'https://ya.ru/');
+values (200, true, 60, 'https://ya.ru/');
 
 insert into rule(expected_status_code, is_activated, period_in_seconds, url)
-values (200, true, 10, 'https://mail.ru/');
+values (200, true, 30, 'https://mail.ru/');
 
 insert into rule(expected_status_code, is_activated, period_in_seconds, url)
-values (200, true, 10, 'https://madbrains.ru/');
+values (200, true, 20, 'https://madbrains.ru/');
 
 insert into rule(expected_status_code, is_activated, period_in_seconds, url)
-values (200, true, 10, 'https://ozon.ru/');
+values (200, true, 40, 'https://ozon.ru/');
 
 insert into rule(expected_status_code, is_activated, period_in_seconds, url)
-values (200, true, 10, 'https://wildberries.ru/');
+values (200, true, 100, 'https://wildberries.ru/');
 
