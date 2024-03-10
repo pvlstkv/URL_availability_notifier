60dd6104f44d51f5a6a366f16c94f17bf8167105
pavel astukov <p7ll7p@google.com>
2024-03-10 21:44:45 +0400

    add readme and little bit fixes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,11 +9,19 @@ values (200, true, 30, 'https://mail.ru/');
 insert into rule(expected_status_code, is_activated, period_in_seconds, url)
 values (200, true, 20, 'https://madbrains.ru/');
 
-insert into rule(expected_status_code, is_activated, period_in_seconds, url)
-values (200, true, 40, 'https://ozon.ru/');
+--
+-- insert into rule(expected_status_code, is_activated, period_in_seconds, url)
+-- values (200, true, 40, 'https://ozon.ru/');
+--
+-- insert into rule(expected_status_code, is_activated, period_in_seconds, url)
+-- values (200, true, 100, 'https://wildberries.ru/');
 
 insert into rule(expected_status_code, is_activated, period_in_seconds, url)
-values (200, true, 100, 'https://wildberries.ru/');
+values (201, true, 20, 'https://mock.codes/200');
+
+
+insert into rule(expected_status_code, is_activated, period_in_seconds, url)
+values (200, true, 20, 'https://mock.codes/404');
 
 
 delete from users;
